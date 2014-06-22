bbcd637337ab5ce7d4ae9d45a741030db5bfebec
Jasper Gregory <jaspergregory@gmail.com>
2014-06-22 14:55:12 -0700

    now runs on heroku

--- app.js
+++ app.js
@@ -57,5 +57,11 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.set('port', process.env.PORT || 5000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
 
 module.exports = app;
