266b15e5a034c03ac68705a79605e00260558378
Jasper Gregory <jaspergregory@gmail.com>
2014-06-23 11:06:24 -0700

    mongoose working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var db = require('./model/db')
+var db = require('./model/db')
 
 var path = require('path');
 var favicon = require('static-favicon');
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-//var storydata = require('../model/story');
+console.log('in users');
+var storydata = require('../model/story');
 
 var mongoose = require( 'mongoose' );
-//Story = mongoose.model('Story');
+Story = mongoose.model('Story');
 
 //exports.index = function (req, res) {
-  // Story.create({
-  //    content : "England",
-  //    group: "D",
-  //    start: '1500-01-01',
-  //    end: null
-  //  }, function(err, story) {
-  //    var strOutput;
-  //    if (err) {
-  //      console.log(err);
-  //      strOutput = 'Oh dear, we\'ve got an error';
-  //    } else {
-  //      console.log('Story created: ' + story);
-  //      strOutput = story.Country + ' created in Group ' + story.group + '\n';
-  //    }
-  //    console.log(strOutput);
-  //  });
+console.log('model created');
+ 
 //};
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+console.log('in get');
+  var strOutput;
+  Story.create({
+       content : "England",
+       group: "D",
+       start: '1500-01-01',
+       end: null
+     }, function(err, story) {
+       console.log('in callback');
+       if (err) {
+         console.log(err);
+         strOutput = 'Oh dear, we\'ve got an error';
+       } else {
+         console.log('Story created: ' + story);
+         strOutput = story.Country + ' created in Group ' + story.group + '\n';
+       }
+       console.log(strOutput);
+       
+     });
+  res.send('respond with a resource : '+ strOutput);
+ 
 });
 
 module.exports = router;
