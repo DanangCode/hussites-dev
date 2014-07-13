4a4f7f06409366a259394160bbffd0cbad08c903
jasper <jaspergregory@gmail.com>
2014-07-13 14:07:38 -0700

    before integration of rest

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
+
+var routes = require('./routes/index');
+var path = require('path');
+var http = require('http');
+var users = require('./routes/users');
+
+var app = express();
+
 var db = require('./model/db')
 
-var path = require('path');
+
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +24,10 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 app.use(cookieParser());
+// app.use(require('stylus').middleware(__dirname + '/public'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,5 @@ console.log('in get');
 
 
 
+
 module.exports = router;
